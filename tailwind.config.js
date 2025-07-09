bd3af013973062b40c279370f8d2463ad90be97a
munki <mikymunki@gmail.com>
2025-07-08 20:10:47 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+colors: {
+  darkgreen: '#013220',
+  darkblue: '#0D1B2A',
+  accentgreen: '#1B5E20',
+  accentblue: '#1976D2',
+  background: '#121212',
+  textprimary: '#FFFFFF',
+  textsecondary: '#B0BEC5',
+}
